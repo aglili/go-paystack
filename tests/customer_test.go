db9b5279b27a981a0bb8c70d7052388f48eb0a03
kweku-xvi <nksarps@gmail.com>
2024-12-05 13:37:28 +0000

    added tests for update customer

--- tests/customer_test.go
+++ tests/customer_test.go
@@ -229,3 +229,60 @@ func TestGetCustomer(t *testing.T) {
 	assert.Equal(t, res.Data.Transaction[0].ID, 1)
 	assert.Equal(t, res.Data.Transaction[1].ID, 2)
 }
+
+
+func TestUpdateCustomer(t *testing.T) {
+	customerCode := "CUS_1234567890"
+
+	Response := paystack.CustomerResponse {
+		Status: true,
+		Message: "Customer updated",
+		Data: struct {
+			ID int `json:"id"`
+			FirstName string `json:"first_name"`
+			LastName string `json:"last_name"`
+			Email string `json:"email"`
+			Phone string `json:"phone"`
+			CustomerCode string `json:"customer_code"`
+			Metadata map[string]interface{} `json:"metadata"`
+		}{
+			ID: 1,
+			CustomerCode: "CUS_1234567890",
+			FirstName: "Jane",
+			LastName: "Smith",
+			Email: "test@test.com",
+			Phone: "54481255651",
+		},
+	}
+
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, r.Method, "PUT")
+		assert.Equal(t, r.URL.Path, fmt.Sprintf("/customer/%s", customerCode))
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(Response)
+	}))
+
+	defer server.Close()
+
+	config.BaseURL = server.URL
+
+	client := paystack.NewClient("sk_test_1234567890")
+
+	req := &paystack.UpdateCustomerRequest{
+		FirstName: "Jane",
+		LastName: "Smith",
+		Phone: "54481255651",
+	}
+
+	res, err := client.UpdateCustomer(customerCode, req)
+	assert.Nil(t, err)
+	assert.NoError(t, err)
+	assert.Equal(t, res.Status, true)
+	assert.Equal(t, res.Message, "Customer updated")
+	assert.Equal(t, res.Data.ID, 1)
+	assert.Equal(t, res.Data.CustomerCode, customerCode)
+	assert.Equal(t, res.Data.FirstName, "Jane")
+	assert.Equal(t, res.Data.LastName, "Smith")
+	assert.Equal(t, res.Data.Phone, "54481255651")
+}
